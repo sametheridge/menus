0387c5970f1c64f8b73dc8a97a8631c2f60d11f3
James Howard <james@plumpdigital.co.uk>
2015-02-25 08:45:19 +0000

    Consistent indenting.

--- code/MenuAdmin.php
+++ code/MenuAdmin.php
@@ -3,8 +3,8 @@
 class MenuAdmin extends ModelAdmin {
 	
 	private static $url_segment = 'menus';
-    private static $menu_title = 'Menus';
-    private static $managed_models = array('Menu');
+	private static $menu_title = 'Menus';
+	private static $managed_models = array('Menu');
 	
 	public $showImportForm = false;
 	
