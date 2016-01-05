9717d0a35bcabdaa5f00de7350fb2214c28aeb76
James Howard <james@plumpdigital.co.uk>
2016-01-05 09:14:05 +0000

    Add method to get menu title from CMS.

--- code/CustomMenu.php
+++ code/CustomMenu.php
@@ -6,14 +6,29 @@ class CustomMenu extends Extension {
 		
 		if ($slug != NULL) {
 			
-			if ($Menu = Menu::get()->filter('Slug', $slug)->First()) {
-				return $Menu->MenuItems();	
+			if ($menu = $this->getMenu($slug)) {
+				return $menu->MenuItems();	
 			} else {
 				return NULL;
 			}
 			
 		}
 		
+	}
+
+	public function CustomMenuTitle($slug = NULL) {
+
+		if ($slug != NULL) {
+			if ($menu = $this->getMenu($slug)) {
+				return $menu->Name;
+			} else {
+				return NULL;
+			}
+		}
+	}
+
+	private function getMenu($slug) {
+		return Menu::get()->filter('Slug', $slug)->First();
 	}
 	
 }
